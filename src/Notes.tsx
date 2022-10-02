c6e7d43553898ee44430c232f2ff370dbb88fa5b
Yann Mainier <yann.mainier@gmail.com>
2022-10-02 22:27:48 +0100

    Use instanced mesh from drei

--- src/Notes.tsx
+++ src/Notes.tsx
@@ -1,18 +1,21 @@
-import { useLayoutEffect, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import type { FC } from "react";
-import { InstancedMesh, Object3D } from "three";
+import { Group } from "three";
 import { useFrame } from "@react-three/fiber";
+import { Instances, Instance } from "@react-three/drei";
 import { minMax } from "./data";
 import type { SongType } from "./data";
 
 type NotesProps = { song: SongType; color: string; objectSize: number };
 export const Notes: FC<NotesProps> = ({ song, color, objectSize }) => {
-  const ref = useRef<InstancedMesh>();
+  const [low, high] = minMax(song);
+  const half = (high - low) / 2;
+  const ref = useRef<Group>();
   const isPausedRef = useRef(false);
 
   useEffect(() => {
     const onKeyDown = ({ code }: KeyboardEvent) => {
-      if (code === 'Space') {
+      if (code === "Space") {
         isPausedRef.current = !isPausedRef.current;
       }
     };
@@ -32,29 +35,18 @@ export const Notes: FC<NotesProps> = ({ song, color, objectSize }) => {
     }
   });
 
-  useLayoutEffect(() => {
-    const object = new Object3D();
-    const [low, high] = minMax(song);
-    const width = high - low;
-    let n = 0;
-    if (!ref.current) return;
-    const half = width / 2;
-    for (let i = 0; i < song.length; i++) {
-      for (let j = 0; j < song[i].length; j++) {
-        const note = song[i][j];
-        object.position.set(note - low - half, 0, -i);
-        object.updateMatrix();
-        ref.current.setMatrixAt(n, object.matrix);
-        n++;
-      }
-    }
-    ref.current.instanceMatrix.needsUpdate = true;
-  }, [song]);
-
   return (
-    <instancedMesh ref={ref} args={[null, null, song.flat().length]}>
-      <boxGeometry args={[objectSize, 0.1, objectSize]} />
-      <meshLambertMaterial color={color} />
-    </instancedMesh>
+    <group ref={ref}>
+      {/* @ts-ignore */}
+      <Instances limit={song.flat().length}>
+        <boxGeometry args={[objectSize, 0.1, objectSize]} />
+        <meshLambertMaterial />
+        {song.flatMap((line, i) =>
+          line.map((note, j) => (
+            <Instance key={`${i}-${j}`} color={color} position={[note - low - half, 0, -i]} />
+          ))
+        )}
+      </Instances>
+    </group>
   );
 };
