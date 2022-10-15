44e43f0aa52a6727d7cbb6ff789b7a6fc2dfdfb2
Yann Mainier <yann.mainier@gmail.com>
2022-10-15 22:23:56 +0100

    remove unused hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { MutableRefObject } from "react";
-import { useThree } from "@react-three/fiber";
 import { KEYCODES_TO_NOTES } from "../data";
 import type { NoteType } from "../data";
 import { Clock } from "../clock";
@@ -99,42 +97,4 @@ export function useTimedNotesQueue(clock: Clock) {
     );
   }, []);
   return timedNotesQueueRef;
-}
-
-type PrevTimedNote = { note: NoteType; start: number; stop?: number };
-
-export function useNotes(): MutableRefObject<
-  Map<NoteType, Array<PrevTimedNote>>
-> {
-  const get = useThree((state) => state.get);
-  const notesMapRef = useRef(new Map<NoteType, Array<PrevTimedNote>>());
-  useEffect(() => {
-    return listen(
-      (note) => {
-        let noteArray = notesMapRef.current.get(note);
-        if (!noteArray) {
-          noteArray = [];
-          notesMapRef.current.set(note, noteArray);
-        }
-        if (
-          noteArray.length === 0 ||
-          noteArray[noteArray.length - 1].stop != null
-        ) {
-          // @ts-ignore
-          const start = get().qClock.getElapsedTime();
-          noteArray.push({ note, start });
-        }
-      },
-      (note) => {
-        let noteArray = notesMapRef.current.get(note);
-        if (noteArray == null || noteArray.length === 0) return;
-        const lastNote = noteArray[noteArray.length - 1];
-        if (lastNote == null || lastNote.stop != null) return;
-        // @ts-ignore
-        const stop = get().qClock.getElapsedTime();
-        lastNote.stop = stop;
-      }
-    );
-  }, [get]);
-  return notesMapRef;
 }
