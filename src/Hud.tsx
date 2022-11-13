bd2eb8fe751321282360f3df5f10d3a005c03385
Yann Mainier <yann.mainier@gmail.com>
2022-11-13 15:53:54 +0000

    score animation

--- src/Hud.tsx
+++ src/Hud.tsx
@@ -1,13 +1,23 @@
 import { FC } from "react";
+import { useSpring, animated } from "react-spring";
 import { useQBeatsStore } from "./store";
 import "./Hud.css";
 
 export const Hud: FC = () => {
   const score = useQBeatsStore((state) => state.score);
+  const style = useSpring({
+    reset: true,
+    from: { rotateY: 0 },
+    to: { rotateY: 720 },
+  });
   return (
     <div className="hud">
       <h1>
-        <span className="score">{score}</span>
+        <span className="score-container">
+          <animated.span style={style} className="score-text">
+            {score}
+          </animated.span>
+        </span>
       </h1>
     </div>
   );
